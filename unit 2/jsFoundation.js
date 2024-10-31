04353de612ad5b23ad1e45a944af4629b6e85024
hshaw11 <158070222+hshaw11@users.noreply.github.com>
2024-10-31 17:41:01 +0000

    JavaScriptFundamentals

---
+++ unit 2/jsFoundation.js
@@ -0,0 +1,29 @@
+// Data types= basic buildings blocks of the JavaScript
+// programming language.
+
+// Strings = data bound together by quotations marks.
+"this is a sentence wrapped in quotes- this is a string"
+"1232321- this is a number but, when in quotes it is a string"
+
+// Integer = numbers- doesn't matter if its a whole number
+// or a decimal
+88678
+789
+1342.785
+0
+
+// boolean = true or false
+true, 1
+false, 0
+
+// variables = are containers for data
+// when writing a variable, we break it down into 3 steps
+// 1. declare the type of variable we're working with
+var a ='// universal container- cant go wrong using this.'
+let b ='// temporary variables- data that will change'
+const c ='// constant variables- data that will NOT change'
+
+let weather_by_city = 'philly'
+let weather_current_temp =80
+let weather_by_day = 'thursday'
+const logo='./weatherchannellogo.jpg'
