e483453db6762660bf6e78e02e70649d62620cb4
hshaw11 <158070222+hshaw11@users.noreply.github.com>
2025-01-08 18:40:25 +0000

    Quarter2Quiz

---
+++ Unit3_Javascript/Quarter2_Quiz.js
@@ -0,0 +1,20 @@
+// Question 1: What is an integer data type? Please write your response in a complete sentence as a string data type.
+// Answer: An interger data type is a representation of a whole number
+
+// Question 2: In your own words, describe what an if/ else conditional statement is. Please write your response in a complete sentence as a string data type.
+// Answer: The if/ else statement helps you to run a specific block of code if the condition is true.
+
+// Question 3: In your own words, describe what a function is. Please write your response in a complete sentence as a string data type.
+// Answer:  modules of code that accomplish a specific task.
+
+// Question 4: What is a boolean data type? Please write your response in a complete sentence as a string data type.
+// Answer:  Yes/No data type which can have two values; Yes (True) or No (False).
+
+// Question 5: In your own words, describe the difference between a function parameter and a function argument.
+// Answer:  A parameter is a variable named in the function or method definition. An argument is the actual value that is passed to the function or method when it is called.
+
+// Question 6: Which operator would work best for the following function: An digital locker function that needs to have a specific set of numbers to be entered in order to lock and unlock a locker in a gym.
+// Answer: The best operator for a digital locker function where a specific set of numbers needs to be entered is the "equal to" operator".
+
+// Question 7: Which operator would work best for the following function: A height verification function that needs to check if a user is a certain height to get onto an amusement park ride.
+// Answer: The best operator for a height verification function would be the greater than or equal to (>=) operator.
